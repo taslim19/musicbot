8b374e12bbcb14ce2307344b2c4ad04762a6b338
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-09-28 11:17:13 +0530

    Update fonts.py

--- AashikaMusicBot/plugins/Aashika/fonts.py
+++ AashikaMusicBot/plugins/Aashika/fonts.py
@@ -1,7 +1,7 @@
 from pyrogram import  filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AashikaMusicBot.utils.annayanx_font import Fonts
+from AashikaMusicBot.utils.aashikafont import Fonts
 from AashikaMusicBot import app
 
 @app.on_message(filters.command(["font", "fonts"]))
