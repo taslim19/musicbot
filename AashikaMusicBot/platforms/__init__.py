35738b3005d8e06f59af91489bc7df185db425b2
Drag <161552347+taslim19@users.noreply.github.com>
2024-10-12 17:20:14 +0530

    Update __init__.py

--- AashikaMusicBot/platforms/__init__.py
+++ AashikaMusicBot/platforms/__init__.py
@@ -2,6 +2,6 @@ from .Apple import AppleAPI
 from .Carbon import CarbonAPI
 from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
-from .Spotify import SpotifyAPI
+from .spotify import SpotifyAPI
 from .Telegram import TeleAPI
 from .Youtube import YouTubeAPI
