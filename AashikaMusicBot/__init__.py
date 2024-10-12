1b70d414aeb63f07a1f2ad21484d48aa5c2cba0b
Drag <161552347+taslim19@users.noreply.github.com>
2024-10-12 17:51:14 +0530

    Update __init__.py

--- AashikaMusicBot/__init__.py
+++ AashikaMusicBot/__init__.py
@@ -3,12 +3,9 @@ from AashikaMusicBot.core.dir import dirr
 from AashikaMusicBot.core.git import git
 from AashikaMusicBot.core.userbot import Userbot
 from AashikaMusicBot.misc import dbb, heroku
-from .spotify import SpotifyAPI
 
 from SafoneAPI import SafoneAPI
 from .logging import LOGGER
-
-from .platforms import *
 
 dirr()
 git()
@@ -20,12 +17,12 @@ api = SafoneAPI()
 userbot = Userbot()
 
 
-
+from .platforms import *
 
 Apple = AppleAPI()
 Carbon = CarbonAPI()
 SoundCloud = SoundAPI()
-spotify = SpotifyAPI()
+Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
