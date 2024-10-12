ca2bb504afa5cf2ed8f57305851254da0c4701b8
Drag <161552347+taslim19@users.noreply.github.com>
2024-10-12 17:52:54 +0530

    Update mongo.py

--- AashikaMusicBot/core/mongo.py
+++ AashikaMusicBot/core/mongo.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
 
-from ..custom_logging import LOGGER
+from ..logging import LOGGER
 
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
