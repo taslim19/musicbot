e8a76a5520602c69f6930cd06d3983297517db37
Drag <161552347+taslim19@users.noreply.github.com>
2024-10-13 15:41:46 +0530

    Update speedtest.py

--- AashikaMusicBot/plugins/tools/speedtest.py
+++ AashikaMusicBot/plugins/tools/speedtest.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
@@ -13,33 +12,36 @@ def testspeed(m, _):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit_text(_["server_12"])
+        m.edit_text(_["server_12"])  # Update message to indicate server selection
         test.download()
-        m = m.edit_text(_["server_13"])
+        m.edit_text(_["server_13"])  # Update message to indicate download test
         test.upload()
-        test.results.share()
-        result = test.results.dict()
-        m = m.edit_text(_["server_14"])
+        test.results.share()  # Share the results for a public link
+        m.edit_text(_["server_14"])  # Update message to indicate completion
+        return test.results.dict()  # Return the results
     except Exception as e:
-        return m.edit_text(f"<code>{e}</code>")
-    return result
+        return m.edit_text(f"<code>{e}</code>")  # Return error message
 
 
 @app.on_message(filters.command(["speedtest", "spt", "st"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 @language 
 async def speedtest_function(client, message: Message, _):
-    m = await message.reply_text(_["server_11"])
+    m = await message.reply_text(_["server_11"])  # Initial message indicating the start of the test
     loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m, _)
-    output = _["server_15"].format(
-        result["client"]["isp"],
-        result["client"]["country"],
-        result["server"]["name"],
-        result["server"]["country"],
-        result["server"]["cc"],
-        result["server"]["sponsor"],
-        result["server"]["latency"],
-        result["ping"],
-    )
-    msg = await message.reply_photo(photo=result["share"], caption=output)
-    await m.delete()
+    result = await loop.run_in_executor(None, testspeed, m, _)  # Run testspeed in a separate thread
+
+    if isinstance(result, dict):  # Ensure result is a dictionary
+        output = _["server_15"].format(
+            result["client"]["isp"],
+            result["client"]["country"],
+            result["server"]["name"],
+            result["server"]["country"],
+            result["server"]["cc"],
+            result["server"]["sponsor"],
+            result["server"]["latency"],
+            result["ping"],
+        )
+        msg = await message.reply_photo(photo=result["share"], caption=output)  # Send the results as a photo
+    else:
+        await m.edit_text(result)  # Edit the initial message to show error
+    await m.delete()  # Delete the initial message
